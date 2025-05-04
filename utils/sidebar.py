d04db4493b0f7fa44cde1bdea2925ec8dc34a408
Caden Lee <caden.finsinyur@gmail.com>
2025-05-04 23:11:06 +0800

    Updated gif

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -5,11 +5,11 @@ def insert_side_bar():
         "© 2025 Finsinyur. All rights reserved."
     )
 
-    linkedin = "https://raw.githubusercontent.com/Finsinyur/Quantvas/blob/main/img/linkedin.gif"
-    main = "https://raw.githubusercontent.com/Finsinyur/Quantvas/blob/main/img/main.gif"
-    email = "https://raw.githubusercontent.com/Finsinyur/Quantvas/blob/main/img/email.gif"
+    linkedin = "https://raw.githubusercontent.com/Finsinyur/Quantvas/main/img/linkedin.gif"
+    main = "https://raw.githubusercontent.com/Finsinyur/Quantvas/main/img/main.gif"
+    email = "https://raw.githubusercontent.com/Finsinyur/Quantvas/main/img/email.gif"
     quantsimplified = (
-        "https://raw.githubusercontent.com/Finsinyur/Quantvas/blob/main/img/blog.gif"
+        "https://raw.githubusercontent.com/Finsinyur/Quantvas/main/img/blog.gif"
     )
 
     st.sidebar.caption(
